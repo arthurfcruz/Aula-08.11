cdc7fb35e090813135113dae3a8e92035108047b
Arthur <arthurfelixcruz@gmail.com>
2022-11-08 20:47:45 -0300

    criando a funcao de escolha

---
+++ teste.js
@@ -0,0 +1,34 @@
+let nome = []
+let senha = []
+
+function cadastrar(){
+    nome = prompt('digite seu nome para cadastro')
+    senha = prompt('digite uma senha para cadastro')
+}
+
+function oQueFazer(){
+    let fazer = parseInt(prompt('O que deseja fazer? 1-cadastrar; 2-fazer login; 3-excluir um cadastro; 4-encerrar programa'))
+    return fazer
+}
+
+do {switch (fazer) {
+        case "1":
+            cadastrar()  
+            oQueFazer()
+            break;
+        case "2":
+            fazerLogin()  
+            oQueFazer()
+            break;
+        case "3":
+            excluirCadastro()   
+            oQueFazer()
+            break;
+        case "4":
+            continuar = 'n'
+            break;
+        default:
+        console.log('essa opcao nao existe')    
+        break;
+    }
+} while (continuar == 's');
