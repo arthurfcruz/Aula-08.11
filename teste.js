9b9ead8a70b50796dc68e9815c5265a35b4eaee3
Arthur <arthurfelixcruz@gmail.com>
2022-11-08 22:31:29 -0300

    funcao excluir cadastro

--- teste.js
+++ teste.js
@@ -1,43 +1,50 @@
-let nome = []
-let senha = []
-let index = []
+let nomes = []
+let senhas = []
+let posicao
 
 function cadastrar(){
-    nome = prompt('digite seu nome para cadastro')
-    senha = prompt('digite uma senha para cadastro')
+    nomes.push(prompt('digite seu nome para cadastro'))
+    senhas.push(prompt('digite uma senha para cadastro'))
 }
 
 function fazerLogin(nome,senha){
-    for (let index = 0; index < nome.length; index++) {
-        if (nome == nome[index] && senha == senha[index]) {
+    for (let index = 0; index < nomes.length; index++) {
+        if (nome == nomes[index] && senha == senhas[index]) {
             return true
         }else 
         return false
     }
 }
 
+function excluirCadastro(nomeExcluido){
+   posicao = nomes.indexOf(nomeExcluido)
+   nomes.splice(posicao,1)
+   senhas.splice(posicao,1)
+   console.log('login excluido com sucesso')
+}
+
 function oQueFazer(){
-    let fazer = parseInt(prompt('O que deseja fazer? 1-cadastrar; 2-fazer login; 3-excluir um cadastro; 4-encerrar programa'))
+    let fazer = prompt('O que deseja fazer? 1-cadastrar; 2-fazer login; 3-excluir um cadastro; 4-encerrar programa')
     return fazer
 }
 
-do {switch (fazer) {
+let continuar = 0
+
+do{let fazer = oQueFazer()
+    switch (fazer) {
         case "1":
             cadastrar()  
-            oQueFazer()
             break;
         case "2":
-            fazerLogin(nome,senha)  
-            if (true) {
+            let nome = prompt('digite um nome para login')
+            let senha = prompt('digite uma senha para login')      
+            if ( fazerLogin(nome,senha) == true) {
                 console.log('login feito com sucesso')
-            }else if (false) {
-                console.log ('usuario ou senha incorrretos')
-            }
-            oQueFazer()
+            }else console.log ('usuario ou senha incorrretos')
             break;
         case "3":
-            excluirCadastro()   
-            oQueFazer()
+            let nomeExcluido = prompt('digite um nome para excluir')    
+            excluirCadastro(nomeExcluido)
             break;
         case "4":
             continuar = 'n'
@@ -46,6 +53,5 @@ do {switch (fazer) {
         console.log('essa opcao nao existe')    
         break;
     }
+    continuar = prompt("deseja continuar? s ou n")
 } while (continuar == 's');
-
-//indexof slice
